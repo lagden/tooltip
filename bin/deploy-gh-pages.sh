20bc8a6e4fd558da1b75d8372728b5aa32e6f95b
Thiago Lagden <lagden@gmail.com>
2015-10-01 21:41:04 -0300

    again

--- bin/deploy-gh-pages.sh
+++ bin/deploy-gh-pages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-rm -rf build
+rm -rf build || exit 0;
 grunt build
 cd build
 git init
@@ -10,3 +10,4 @@ git config user.email "travis@lagden.in"
 git add .
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+exit 0
